e5465dfc26a07328a73bdae7e16f68c359527fdf
mashijifu <4ts0t0i7c@gmail.com>
2024-05-03 15:25:24 +0900

    20240503作業

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,8 @@ class Book < ApplicationRecord
   # nilを返す必要がある場合は、scopeではなくクラスメソッドを使う
   scope :costly, -> { where("price > ?", 3000) }
   scope :written_about, ->(theme) { where("name like ?", "%#{theme}%") }
+
+  belongs_to :publisher
+  has_many :book_authors
+  has_many :authors, through: :book_authors
 end
