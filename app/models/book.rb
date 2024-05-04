0158faa241505adbc9b6297c2681f82fbce9688d
mashijifu <4ts0t0i7c@gmail.com>
2024-05-04 11:21:01 +0900

    バリデーションについて

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,4 +6,18 @@ class Book < ApplicationRecord
   belongs_to :publisher
   has_many :book_authors
   has_many :authors, through: :book_authors
+
+  # railsコンドールでバリデーションエラーを確認する方法
+  # errorsメソッドを使う(例：book.errors)
+  # full_messagesメソッドを使う（例：book.errors.full_messages）
+  # 保存をせずにバリデーションを確認する方法
+  # valid?メソッドを使う（例：book.valid?）バリデーションをすべて通過すればtrue、どこかで失敗すればfalseを返す
+  # ActiveRecordの!有無の違い
+  # 「!」を伴なわないメソッドはバリデーション失敗時に例外を起こさない
+  # 「!」を伴なうメソッドはバリデーション失敗時には例外「ActiveRecord::RecordInvalid」が起こる
+
+  # 以下のバリデーションは、nameが空でないこと、nameが25文字以内であること、priceが0以上であることを確認する
+  validates :name, presence: true
+  validates :name, length: { maximum: 25 }
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
 end
