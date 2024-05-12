05f33656384376c177bc600d6825f55d35a84394
mashijifu <4ts0t0i7c@gmail.com>
2024-05-12 13:16:40 +0900

    5/12(日)作業分

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,21 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/books/:id" => "books#show"
   delete "/books/:id" => "books#destroy"
+  resources :publishers
+  # resourceは、indexアクションを除き、:idによる絞り込みも行わない
+  resource :profile
+  # resource :profile, only: %i{show, edit, update}
+
+  # resourcesは、全てのアクションを自動生成する
+  # resources :publishers do
+  #   resources :books
+  #   # memberはidを指定してアクセスする
+  #   member do
+  #     get "detail"
+  #   end
+  #   # collectionはidを指定せずにアクセスする
+  #   collection do
+  #     get "search"
+  #   end
+  # end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_05_07_150051) do
+ActiveRecord::Schema.define(version: 2024_05_12_040056) do
 
   create_table "authors", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
@@ -42,6 +42,14 @@ ActiveRecord::Schema.define(version: 2024_05_07_150051) do
   create_table "publishers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.text "address"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.string "password"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
