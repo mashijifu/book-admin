c79bcd1f066b5d974ab2f8c022069e6959ebe94c
mashijifu <4ts0t0i7c@gmail.com>
2024-05-13 21:43:15 +0900

    5/13(月)作業分

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,10 +8,11 @@ class BooksController < ApplicationController
   around_action :action_logger, only: [:destroy]
 
   def show
-    respond_to do |format|
-      format.html
-      format.json
-    end
+    # respond_to do |format|
+    #   format.html
+    #   format.json
+    # end
+    # render :show
   end
 
   def destroy
